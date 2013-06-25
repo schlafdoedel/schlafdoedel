44ca056416a077d84bccc0286b3b68a038249ad1
Masterclaus <claus.schabetsberger@gmail.com>
2013-06-25 11:08:13 +0200

    Added context menu items

--- src/com/kg6/schlafdoedel/ContextMenu.java
+++ src/com/kg6/schlafdoedel/ContextMenu.java
@@ -13,37 +13,73 @@ import com.kg6.schlafdoedel.network.BluetoothConnection;
 public class ContextMenu {
 	
 	private enum ContextMenuOptions {
-		SLEEPING_PHASES_SIMULATION,
+		SIMULATE_SLEEPING_PHASES,
+		SIMULATE_WEATHER_REQUEST,
+		SIMULATE_NEWS_REQUEST,
 		CLOSE_APPLICATION
 	};
 	
 	public static void Create(Menu menu, Context context) {
-		menu.add(0, ContextMenuOptions.SLEEPING_PHASES_SIMULATION.ordinal(), ContextMenuOptions.SLEEPING_PHASES_SIMULATION.ordinal(), "Simulate sleeping phases");
+		menu.add(0, ContextMenuOptions.SIMULATE_SLEEPING_PHASES.ordinal(), ContextMenuOptions.SIMULATE_SLEEPING_PHASES.ordinal(), "Simulate sleeping phases");
+		menu.add(0, ContextMenuOptions.SIMULATE_WEATHER_REQUEST.ordinal(), ContextMenuOptions.SIMULATE_WEATHER_REQUEST.ordinal(), "Simulate weather request");
+		menu.add(0, ContextMenuOptions.SIMULATE_NEWS_REQUEST.ordinal(), ContextMenuOptions.SIMULATE_NEWS_REQUEST.ordinal(), "Simulate news request");
 		menu.add(0, ContextMenuOptions.CLOSE_APPLICATION.ordinal(), ContextMenuOptions.CLOSE_APPLICATION.ordinal(), "Close");
 	}
 	
 	public static void ManageItemClick(MenuItem item, Activity context) {
-		if(item.getItemId() == ContextMenuOptions.SLEEPING_PHASES_SIMULATION.ordinal()) {
-			SleepingPhaseDialog dialog = new SleepingPhaseDialog(context);
-			dialog.show();
-		} else if(item.getItemId() == ContextMenuOptions.CLOSE_APPLICATION.ordinal()) {
-			try {
-				BluetoothConnection bluetoothConnection = BluetoothConnection.CreateInstance(context);
+		ContextMenuOptions option = ContextMenuOptions.values()[item.getItemId()];
+		
+		switch(option) {
+			case SIMULATE_SLEEPING_PHASES:
+				simulateSleepingPhases(context);
 				
-				bluetoothConnection.cleanup();
-			} catch (Exception e) {
-				Log.e("ContextMenu.java", "Unable to cleanup the Bluetooth connection", e);
-			}
-			
-			try {
-				EventScheduler eventScheduler = EventScheduler.CreateInstance(context, null);
+				break;
+			case SIMULATE_WEATHER_REQUEST:
+				simulateWeatherRequest();
 				
-				eventScheduler.cleanup();
-			} catch (Exception e) {
-				Log.e("ContextMenu.java", "Unable to cleanup the EventScheduler", e);
-			}
-			
-			System.exit(0);
+				break;
+			case SIMULATE_NEWS_REQUEST:
+				simulateNewsRequest();
+				
+				break;
+			case CLOSE_APPLICATION:
+				closeApplication(context);
+				
+				break;
 		}
+	}
+	
+	private static void simulateSleepingPhases(Activity context) {
+		SleepingPhaseDialog dialog = new SleepingPhaseDialog(context);
+		
+		dialog.show();
+	}
+	
+	private static void simulateWeatherRequest() {
+		//TODO
+	}
+	
+	private static void simulateNewsRequest() {
+		//TODO
+	}
+	
+	private static void closeApplication(Activity context) {
+		try {
+			BluetoothConnection bluetoothConnection = BluetoothConnection.CreateInstance(context);
+			
+			bluetoothConnection.cleanup();
+		} catch (Exception e) {
+			Log.e("ContextMenu.java", "Unable to cleanup the Bluetooth connection", e);
+		}
+		
+		try {
+			EventScheduler eventScheduler = EventScheduler.CreateInstance(context, null);
+			
+			eventScheduler.cleanup();
+		} catch (Exception e) {
+			Log.e("ContextMenu.java", "Unable to cleanup the EventScheduler", e);
+		}
+		
+		System.exit(0);
 	}
 }
